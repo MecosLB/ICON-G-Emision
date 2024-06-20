9753a0005d5995f068902aa5c70c90f225caa7c5
Fernando A Jacobo <fernandojacobo54@gmail.com>
2024-06-20 09:20:34 -0600

    Dasboard

--- src/modules/dashboard/views/IssueCfdi.vue
+++ src/modules/dashboard/views/IssueCfdi.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
+import Multiselect from '@vueform/multiselect';
+
 import LoaderView from '@/modules/dashboard/components/LoaderView.vue';
 import LoaderData from '@/modules/dashboard/components/LoaderData.vue';
 import Alert from '@/modules/dashboard/components/Alert.vue';
@@ -11,12 +13,27 @@ onMounted(async () => {
     await loadView();
 });
 
+const isNational = ref(true);
+
 const loadView = async () => {
     showView.value = true;
 }
 
+const nacionalidades = ref([
+    { label: 'Nacional', value: 'Nacional' },
+    { label: 'Extranjero', value: 'Extranjero' },
+]);
+
+const selectNacionalidad = (value) => {
+    if (value == 'Nacional') isNational.value = true;
+    if (value == 'Extranjero') isNational.value = false;
+    if (value == null) isNational.value = true;
+}
+
 const showView = ref(false);
 </script>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
 
 <template>
     <LoaderView v-show="!showView" />
@@ -75,22 +92,22 @@ const showView = ref(false);
                 <div class="p-2">
                     <div class="row">
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Folio </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
 
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Fecha </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
 
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Cve. Retención </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
 
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Descripción </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
                     </div>
@@ -99,10 +116,36 @@ const showView = ref(false);
 
             <div class="tab-pane fade" id="cfdisRelacionados-tab-pane" role="tabpanel" aria-labelledby="cfdisRelacionados-tab" tabindex="1">
                 <div class="p-2">
-                    <div class="row">
+                    <div class="row mb-4">
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Tipo de Relación </label>
                             <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> U.U.I.D. </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4 d-flex align-items-center p-2">
+                            <button type="button" class="btn btn-sm btn-primary">
+                                <i class="fa-solid fa-plus"></i>
+                                Agregar
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table table-sm table-striped table-hover">
+                                <thead>
+                                    <tr class="text-center">
+                                        <th scope="col"> Tipo de Relación </th>
+                                        <th scope="col"> U.U.I.D. </th>
+                                        <th scope="col"> </th>
+                                    </tr>
+                                </thead>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -110,9 +153,37 @@ const showView = ref(false);
 
             <div class="tab-pane fade" id="receptor-tab-pane" role="tabpanel" aria-labelledby="receptor-tab" tabindex="2">
                 <div class="p-2">
-                    <div class="row">
+                    <div class="row mb-2">
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold mb-1"> Nacionalidad </label>
+                            <Multiselect :options="nacionalidades"
+                                :searchable="true" trackBy="label" label="label" @change="selectNacionalidad" />
+                        </div>
+                    </div>
+
+                    <div class="row mb-2">
+                        <div v-if="isNational" class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> R.F.C. </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Nombre  / Razon Social </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div v-if="isNational" class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> C.U.R.P. </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div v-if="isNational" class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Domicilio Fiscal </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div v-if="!isNational" class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Num. Reg. ID Trib. </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
                     </div>
@@ -121,9 +192,19 @@ const showView = ref(false);
 
             <div class="tab-pane fade" id="periodo-tab-pane" role="tabpanel" aria-labelledby="periodo-tab" tabindex="3">
                 <div class="p-2">
-                    <div class="row">
+                    <div class="row mb-2">
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Mes Inicio </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Mes Fin </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Ejercicio </label>
                             <input type="text" class="form-control form-control-sm">
                         </div>
                     </div>
@@ -132,17 +213,57 @@ const showView = ref(false);
 
             <div class="tab-pane fade" id="totales-tab-pane" role="tabpanel" aria-labelledby="totales-tab" tabindex="4">
                 <div class="p-2">
-                    <div class="row">
+                    <div class="row mb-2">
                         <div class="col-12 col-sm-12 col-md-4">
-                            <label for="" class="fw-bold"> Label </label>
+                            <label for="" class="fw-bold"> Monto Total Operacion </label>
                             <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Monto Total Gravado </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Monto Total Exento </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Monto Total Retenido </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> Utilidad Bimestral </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+
+                        <div class="col-12 col-sm-12 col-md-4">
+                            <label for="" class="fw-bold"> I.S.R. Correspondiente </label>
+                            <input type="text" class="form-control form-control-sm">
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="table-responsive">
+                            <table class="table table-sm table-striped table-hover">
+                                <thead>
+                                    <tr class="text-center">
+                                        <th scope="col"> Base </th>
+                                        <th scope="col"> Impuesto </th>
+                                        <th scope="col"> Monto </th>
+                                        <th scope="col"> Tipo Pago </th>
+                                    </tr>
+                                </thead>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="d-flex align-items-center justify-content-center gap-3">
+        <div class="d-flex align-items-center justify-content-center gap-3 mt-4">
             <button type="button" class="btn btn-sm btn-primary">
                 Emitir Comprobante
             </button>
